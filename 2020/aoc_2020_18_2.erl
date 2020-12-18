b54ec48461ba9eb574318facf29a376e92081e76
fredfsh <fredfsh@gmail.com>
2020-12-17 21:58:54 -0800

    2020 day 18

---
+++ 2020/aoc_2020_18_2.erl
@@ -0,0 +1,60 @@
+-module(aoc_2020_18_2).
+
+-export([start/0]).
+
+start() ->
+    Out = run(),
+    io:format("~p~n", [Out]),
+    ok.
+
+run() ->
+    run_impl(0).
+
+run_impl(Acc) ->
+    case input() of
+        eof ->
+            Acc;
+        X ->
+            run_impl(Acc + do(X))
+    end.
+
+input() ->
+    case io:get_line("") of
+        eof ->
+            eof;
+        L ->
+            L
+    end.
+
+do(X) ->
+    do_impl(X, [], []).
+
+-define(SPACE, 32).
+-define(NEW_LINE, 10).
+
+do_impl([?NEW_LINE], Numbers, Ops) ->
+    {[Acc], []} = calc_all(Numbers, Ops),
+    Acc;
+do_impl([?SPACE | T], Numbers, Ops) ->
+    do_impl(T, Numbers, Ops);
+do_impl([X | _] = S, Numbers, Ops) when X >= $0, X =< $9 ->
+    {N, Rem} = string:to_integer(S),
+    {NewNumbers, NewOps} = calc([N | Numbers], Ops),
+    do_impl(Rem, NewNumbers, NewOps);
+do_impl([$) | T], Numbers, Ops) ->
+    {NewNumbers, NewOps} = calc_all(Numbers, Ops),
+    do_impl(T, NewNumbers, NewOps);
+do_impl([Op | T], Numbers, Ops) ->
+    do_impl(T, Numbers, [Op | Ops]).
+
+calc([L, R | NT], [$+ | OT]) ->
+    calc([L + R | NT], OT);
+calc(Numbers, Ops) ->
+    {Numbers, Ops}.
+
+calc_all(Numbers, [$( | OT]) ->
+    calc(Numbers, OT);
+calc_all(Numbers, []) ->
+    {Numbers, []};
+calc_all([L, R | NT], [$* | OT]) ->
+    calc_all([L * R | NT], OT).
