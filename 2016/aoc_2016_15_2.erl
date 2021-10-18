61fdc6dc3057376480816eb84f9aa8903a823926
fredfsh <fredfsh@gmail.com>
2021-10-18 16:00:56 -0700

    2016 day 15

---
+++ 2016/aoc_2016_15_2.erl
@@ -0,0 +1,51 @@
+-module(aoc_2016_15_2).
+
+-export([start/0]).
+
+start() ->
+    Out = run(),
+    io:format("~p~n", [Out]),
+    ok.
+
+run() ->
+    do(input()).
+
+input() ->
+    extra(input_impl([])).
+
+input_impl(Acc) ->
+    Format = "Disc #~d has ~d positions; at time=0, it is at position ~d.",
+    case io:fread("", Format) of
+        eof ->
+            Acc;
+        {ok, [I, N, P]} ->
+            input_impl([{I, P, N} | Acc])
+    end.
+
+extra(L) ->
+    [{length(L) + 1, 0, 11} | L].
+
+do(X) ->
+    do_impl(0, 1, X).
+
+do_impl(X, _LCD, []) ->
+    X;
+do_impl(X, LCD, [{I, P, N} | T]) ->
+    Y = inc(X, LCD, I, P, N),
+    NewLCD = lcd(LCD, N),
+    do_impl(Y, NewLCD, T).
+
+inc(X, _LCD, I, P, N) when (X + I + P) rem N =:= 0 ->
+    X;
+inc(X, LCD, I, P, N) ->
+    inc(X + LCD, LCD, I, P, N).
+
+lcd(X, Y) ->
+    X * Y div gcd(X, Y).
+
+gcd(X, Y) when X < Y ->
+    gcd(Y, X);
+gcd(X, Y) when X rem Y =:= 0 ->
+    Y;
+gcd(X, Y) ->
+    gcd(Y, X rem Y).
