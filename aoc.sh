0d99ee7d5f1df44553315dc4308babf56572d975
fredfsh <fredfsh@gmail.com>
2021-11-04 16:26:12 -0700

    aoc.sh: fix dir

--- aoc.sh
+++ aoc.sh
@@ -1,15 +1,19 @@
+DIR=~/AdventOfCode
+
 aoc_git_push() {
-    OUT=`cd ~/AdventOfCode && git status --porcelain | head -1`
+    OUT=`cd $DIR && git status --porcelain | head -1`
     YEAR=`echo $OUT | cut -d '_' -f 2`
     DAY=`echo $OUT | cut -d '_' -f 3`
 
     if (( YEAR >= 2015 && DAY >= 1 && DAY <= 25 )); then
+        pushd $DIR
         git add $YEAR
         git commit -a -m "$YEAR day $DAY"
         git show
         if read -q "choice?Push commit to remote? [y/n]"; then
             git push origin master
         fi
+        popd
     else
         echo "Unable to parse git output:"
         echo $OUT
