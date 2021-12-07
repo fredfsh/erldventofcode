21a0723927169e0537d8d2556bf455b7add8e59c
fredfsh <fredfsh@gmail.com>
2021-12-06 21:07:36 -0800

    2021 day 7

---
+++ 2021/aoc_2021_7_1.erl
@@ -0,0 +1,44 @@
+-module(aoc_2021_7_1).
+
+-export([start/0]).
+
+start() ->
+    Out = run(),
+    io:format("~p~n", [Out]),
+    ok.
+
+run() ->
+    run_impl(ini()).
+
+run_impl(Acc) ->
+    case input() of
+        eof ->
+            fin(Acc);
+        X ->
+            run_impl(acc(Acc, do(X)))
+    end.
+
+input() ->
+    case io:fread("", "~s") of
+        eof ->
+            eof;
+        {ok, [L]} ->
+            lists:sort([list_to_integer(X) || X <- string:split(L, ",", all)])
+    end.
+
+ini() ->
+    0.
+
+do(L) ->
+    Min = hd(L),
+    Max = lists:last(L),
+    hd(lists:sort([fuel(X, L) || X <- lists:seq(Min, Max)])).
+
+fuel(X, L) ->
+    lists:sum([abs(X - Y) || Y <- L]).
+
+acc(Acc, X) ->
+    Acc + X.
+
+fin(X) ->
+    X.
