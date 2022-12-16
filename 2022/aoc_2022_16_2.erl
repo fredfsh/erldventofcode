b3acc09544ac62e3bab089549af5b5fc6718a6ec
fredfsh <fredfsh@gmail.com>
2022-12-15 23:03:23 -0800

    2022 day 16

---
+++ 2022/aoc_2022_16_2.erl
@@ -0,0 +1,127 @@
+-module(aoc_2022_16_2).
+
+-export([start/0]).
+
+start() ->
+    Out = run(),
+    io:format("~p~n", [Out]),
+    ok.
+
+run() ->
+    run_impl(ini()).
+
+run_impl(Acc) ->
+    case input() of
+        eof ->
+            fin(Acc);
+        X ->
+            run_impl(acc(Acc, do(X)))
+    end.
+
+input() ->
+    Fmt = "Valve ~a has flow rate=~d; ~s ~s to ~s",
+    case io:fread("", Fmt) of
+        eof ->
+            eof;
+        {ok, [Valve, Flowrate, _, _, _]} ->
+            Line = string:trim(io:get_line("")),
+            Parts = string:split(Line, ", ", all),
+            {Valve, Flowrate, [list_to_atom(P) || P <- Parts]}
+    end.
+
+ini() ->
+    {sets:new(), maps:new(), sets:new(), maps:new()}.
+
+do(X) ->
+    X.
+
+acc({Valves, Flowrates, PipeSets, PipeMaps}, {Valve, Flowrate, Dests}) ->
+    NFlowrates = case Flowrate of
+                     0 ->
+                         Flowrates;
+                     _ ->
+                         maps:put(Valve, Flowrate, Flowrates)
+                 end,
+    {sets:add_element(Valve, Valves),
+     NFlowrates,
+     sets:union(sets:from_list([{Valve, D} || D <- Dests]), PipeSets),
+     maps:put(Valve, Dests, PipeMaps)}.
+
+-define(MINUTES, 26).
+-define(START, 'AA').
+
+-define(TAB, memo).
+
+fin({Valves, Flowrates, _PipeSets, PipeMaps}) ->
+    Steps = steps(Valves, PipeMaps),
+    ets:new(?TAB, [named_table]),
+    dfs(Flowrates, ?MINUTES, ?START, 0, false, Steps).
+
+steps(Valves, PipeMaps) ->
+    F = fun(Valve, Acc) ->
+                steps_impl(queue:from_list([{Valve, 0}]),
+                           sets:from_list([Valve]),
+                           Acc,
+                           Valve,
+                           PipeMaps)
+        end,
+    sets:fold(F, maps:new(), Valves).
+
+steps_impl(Q, Visited, Steps, From, PipeMaps) ->
+    case queue:out(Q) of
+        {empty, _} ->
+            Steps;
+        {{value, {Valve, N}}, Q2} ->
+            F = fun(Dest, {QAcc, VAcc, StepsAcc} = Acc) ->
+                        case sets:is_element(Dest, VAcc) of
+                            true ->
+                                Acc;
+                            false ->
+                                {queue:in({Dest, N + 1}, QAcc),
+                                 sets:add_element(Dest, VAcc),
+                                 maps:put({From, Dest}, N + 1, StepsAcc)}
+                        end
+                end,
+            {NQ, NV, NSteps} = lists:foldl(F,
+                                           {Q2, Visited, Steps},
+                                           maps:get(Valve, PipeMaps, [])),
+            steps_impl(NQ, NV, NSteps, From, PipeMaps)
+    end.
+
+dfs(Flowrates, Minutes, Current, Pressure, Elephant, Steps) ->
+    F = fun(Valve, Flowrate, Acc) ->
+                case Minutes - maps:get({Current, Valve}, Steps) - 1 of
+                    Rem when Rem > 0, Elephant ->
+                        max(Acc, dfs(maps:remove(Valve, Flowrates),
+                                     Rem,
+                                     Valve,
+                                     Pressure + Flowrate * Rem,
+                                     Elephant,
+                                     Steps));
+                    Rem when Rem > 0 ->
+                        max(
+                          max(Acc, dfs(maps:remove(Valve, Flowrates),
+                                       Rem,
+                                       Valve,
+                                       Pressure + Flowrate * Rem,
+                                       Elephant,
+                                       Steps)),
+                          Pressure + memo(Flowrates, Steps));
+                    _ when Elephant ->
+                        Acc;
+                    _ ->
+                        max(Acc, Pressure + memo(Flowrates, Steps))
+                end
+        end,
+    maps:fold(F, Pressure, Flowrates).
+
+memo(Flowrates, Steps) ->
+    Key = lists:sort(maps:keys(Flowrates)),
+    case ets:lookup(?TAB, Key) of
+        [] ->
+            Res = dfs(Flowrates, ?MINUTES, ?START, 0, true, Steps),
+            ets:insert(?TAB, {Key, Res}),
+            Res;
+        [{_, Val}] ->
+            Val
+    end.
