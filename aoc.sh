54fb7a1a7515594650292a8e216aad4b9a2d8bc1
Shi Feng <fredfsh@gmail.com>
2023-12-04 21:12:41 -0800

    aoc.sh: run directly

--- aoc.sh
+++ aoc.sh
@@ -1,21 +1,19 @@
 DIR=~/erldventofcode
 
-aoc_git_push() {
-    OUT=`cd $DIR && git status --porcelain | head -1`
-    YEAR=`echo $OUT | cut -d '_' -f 2`
-    DAY=`echo $OUT | cut -d '_' -f 3`
+OUT=`cd $DIR && git status --porcelain | head -1`
+YEAR=`echo $OUT | cut -d '_' -f 2`
+DAY=`echo $OUT | cut -d '_' -f 3`
 
-    if (( YEAR >= 2015 && DAY >= 1 && DAY <= 25 )); then
-        pushd $DIR
-        git add $YEAR
-        git commit -a -m "$YEAR day $DAY"
-        git show
-        if read -q "choice?Push commit to remote? [y/n]"; then
-            git push origin master
-        fi
-        popd
-    else
-        echo "Unable to parse git output:"
-        echo $OUT
+if (( YEAR >= 2015 && DAY >= 1 && DAY <= 25 )); then
+    pushd $DIR
+    git add $YEAR
+    git commit -a -m "$YEAR day $DAY"
+    git show
+    if read -q "choice?Push commit to remote? [y/n]"; then
+        git push origin master
     fi
-}
+    popd
+else
+    echo "Unable to parse git output:"
+    echo $OUT
+fi
