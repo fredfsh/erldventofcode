fd192795f6ee07a479e4077e3d1b48d8f6dde65a
Shi Feng <fredfsh@gmail.com>
2024-12-06 21:30:18 -0800

    2024 day 7

---
+++ 2024/aoc_2024_7_2.erl
@@ -0,0 +1,77 @@
+-module(aoc_2024_7_2).
+
+-export([start/0]).
+
+start() ->
+    Out = run(),
+    io:format("~p~n", [Out]),
+    ok.
+
+run() ->
+    run_impl(ini()).
+
+run_impl(Acc) ->
+    case input() of
+        eof ->
+            fin(Acc);
+        X ->
+            run_impl(acc(Acc, do(X)))
+    end.
+
+input() ->
+    case io:get_line("") of
+        eof ->
+            eof;
+        L ->
+            Strs = string:lexemes(string:trim(L, trailing), ": "),
+            [Val | Ops] = [list_to_integer(S) || S <- Strs],
+            {Val, Ops}
+    end.
+
+ini() ->
+    0.
+
+do({Val, Ops}) ->
+    case eq(Val, Ops) of
+        true ->
+            Val;
+        false ->
+            0
+    end.
+
+eq(Val, Ops) ->
+    eq_impl(Val, lists:reverse(Ops)).
+
+eq_impl(X, [Y]) ->
+    X =:= Y;
+eq_impl(X, [H | _]) when X < H ->
+    false;
+eq_impl(X, [H | T]) ->
+    eq_impl(X - H, T) orelse (X rem H =:= 0 andalso eq_impl(X div H, T))
+        orelse concatable(X, H, T).
+
+concatable(X, X, _) ->
+    false;
+concatable(X, H, T) ->
+    case trim_suffix(X, H) of
+        undefined ->
+            false;
+        Prefix ->
+            eq_impl(Prefix, T)
+    end.
+
+trim_suffix(X, Suffix) ->
+    Prefix = lists:reverse(integer_to_list(Suffix)),
+    Rev = lists:reverse(integer_to_list(X)),
+    case string:prefix(Rev, Prefix) of
+        nomatch ->
+            undefined;
+        Rem ->
+            list_to_integer(lists:reverse(Rem))
+    end.
+
+acc(Acc, X) ->
+    Acc + X.
+
+fin(X) ->
+    X.
